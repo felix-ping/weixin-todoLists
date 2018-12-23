2e4a8efc140b1d0c9f68dcb5cfa72a079609d7c4
ping <513011631@qq.com>
2018-12-23 15:12:17 +0800

    添加了滑动删除功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,93 +1,107 @@
 const pageData = {
-    data: {
-        taskList: [],
+  data: {
+    taskList: [],
+    task: '',
+    tipsHidden: true,
+    pageXStart:0
+  },
+
+  onShow: function () {
+    this.setData({
+      taskList: wx.getStorageSync('taskList') || []
+    });
+  },
+
+  bindTask: function (e) {
+    this.setData({
+      task: e.detail.value
+    });
+  },
+
+  toCreate: function () {
+    let taskList = this.data.taskList;
+    if (this.data.task.trim().length < 1) {
+      this.toShowTips();
+    } else {
+      taskList.unshift({
+        id: Date.now(),
+        text: this.data.task,
+        done: false
+      });
+      this.setData({
         task: '',
-        tipsHidden: true 
-    },
+        taskList: taskList
+      });
+      wx.setStorageSync('taskList', taskList);
+    }
 
-    onShow: function () {
-        this.setData({
-            taskList: wx.getStorageSync('taskList') || []
-        });
-    },
+  },
 
-    bindTask: function (e) {
-        this.setData({
-            task: e.detail.value
-        });
-    },
+  toggleDone: function (e) {
+    let id = e.currentTarget.id;
+    let modify = this.data.taskList.map((item) => {
+      return item.id == id ? Object.assign({}, item, { done: !item.done }) : item;
+    });
+    this.setData({
+      'taskList': modify
+    });
+    wx.setStorageSync('taskList', modify);
+  },
 
-    toCreate: function () {
-        let taskList = this.data.taskList;
-        if (this.data.task.trim().length < 1) {
-            this.toShowTips();
-        } else {
-            taskList.unshift({
-                id: Date.now(),
-                text: this.data.task,
-                done: false
-            });
-            this.setData({
-                task: '',
-                taskList: taskList
-            });
-            wx.setStorageSync('taskList', taskList);
-        }
-
-    },
-
-    toggleDone: function (e) {
-        let id = e.currentTarget.id;
-        let modify = this.data.taskList.map((item) => {
-            return item.id == id ? Object.assign({}, item, { done: !item.done }) : item;
-        });
-        this.setData({
-            'taskList': modify
-        });
-        wx.setStorageSync('taskList', modify);
-    },
-
-    toRemove: function (e) {
-      let id = e.currentTarget.id;
-      wx.showModal({
+  toRemove: function (e) {
+    let id = e.currentTarget.id;
+    wx.showModal({
       title: '警告',
       content: '您确定要删除么？',
-      confirmText:'删除',
-      confirmColor:'#ff0000',
-      success:(res) => {
-      if (res.confirm) {
-        let modify = this.data.taskList.filter((item) => {
-          return item.id != id;
-        });
-        this.setData({
-          'taskList': modify
-        });
-        wx.setStorageSync('taskList', modify);
-      } else if (res.cancel) {
-        console.log('用户点击取消')
+      confirmText: '删除',
+      confirmColor: '#ff0000',
+      success: (res) => {
+        if (res.confirm) {
+          let modify = this.data.taskList.filter((item) => {
+            return item.id != id;
+          });
+          this.setData({
+            'taskList': modify
+          });
+          wx.setStorageSync('taskList', modify);
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
       }
+    })
+
+
+
+  },
+
+  toHideTips: function () {
+    this.setData({
+      tipsHidden: true
+    });
+  },
+
+  toShowTips: function () {
+    this.setData({
+      tipsHidden: false
+    });
+  },
+
+  confirmRemove: function () {
+
+  },
+
+  touchStart: function(e){
+    this.setData({ pageXStart: e.changedTouches[0].pageX});
+  },
+
+  touchEnd:  function(e) {
+    let pageXMove = (e.changedTouches[0].pageX) - this.data.pageXStart
+    if (Math.abs(pageXMove) > 35){
+      console.log(pageXMove)
+      this.toRemove(e)
     }
-})
-        
-        
-        
-    },
-
-    toHideTips: function () {
-        this.setData({
-            tipsHidden: true
-        });
-    },
-
-    toShowTips: function () {
-        this.setData({
-            tipsHidden: false
-        });
-    },
     
-    confirmRemove:function(){
-
-    }
+  }
 };
 
 Page(pageData);
